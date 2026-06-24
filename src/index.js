b542c982522cfdbc205a8d5c2426715d919c0e73
Kaushik Mudaliyar <kaushikmudaliyar1484@gmail.com>
2026-06-24 16:14:09 +0530

    connect to mongodb database

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/db.js";
 dotenv.config({
   path: "./.env",
 });
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server is listening on http://localhost:${port}`);
-});
+connectDB()
+  .then(
+    app.listen(port, () => {
+      console.log(`Server is listening on http://localhost:${port}`);
+    }),
+  )
+  .catch((err) => console.log("MongoDB connection error", err));

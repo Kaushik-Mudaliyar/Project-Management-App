10bbfeebb15c65670d60015a7cb0329e886d7f0e
Kaushik Mudaliyar <kaushikmudaliyar1484@gmail.com>
2026-06-24 16:26:44 +0530

    add asyncHanlder utility and edit healtcheck controller

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,13 +1,19 @@
 import { ApiResponse } from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+// const healthCheck = (req, res, next) => {
+//   try {
+//     return res
+//       .status(200)
+//       .json(new ApiResponse(200, { message: "Server is running" }));
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-const healthCheck = (req, res, next) => {
-  try {
-    return res
-      .status(200)
-      .json(new ApiResponse(200, { message: "Server is running" }));
-  } catch (error) {
-    next(error);
-  }
-};
+const healthCheck = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { message: "Server is running" }));
+});
 
 export { healthCheck };
